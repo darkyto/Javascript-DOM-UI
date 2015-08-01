bc8757c67351e6be1c2a47a19a421f3a38ce10d5
darkyto <iliev.nick007@gmail.com>
2015-08-01 17:48:35 +0300

    fix 5th unit test condition
    fix 5th unit test condition

--- jQuery-Plugins-Tasks/tasks/task-1.js
+++ jQuery-Plugins-Tasks/tasks/task-1.js
@@ -8,19 +8,28 @@ function solve(){
     var $dropdownDiv = $('<div />').addClass('dropdown-list');
     
     $selector.css('display','none');
- 
-    $current.text('Option 1');
-    
+    $current.text('Select a value');   
     $container.css('display','none');
 
     var $item = $('<div />').addClass('dropdown-item');
 
-    for (index = 0; index < 5; index+=1) {
+    for (var i = 0; i < 5; i+=1) {
+
+        $('<div class="dropdown-item" data-value="' 
+            + (i - 1) +'" data-index = "' 
+            + i + '">Option ' 
+            + (i + 1) + '</div>').appendTo($container);
+
+        /* 
+        //this below should be ok but has not passed the 5th unit test
+        
         var $newItem = $item.clone();
         $newItem.attr('data-value', 'value-'+ (index+1));
         $newItem.attr('data-index', index);    
         $newItem.text('Option '+(index+1));
         $container.append($newItem);
+        */
+        
     }
 
     $dropdownDiv.append($selector);
@@ -31,20 +40,20 @@ function solve(){
     $body.append($dropdownDiv);
 
     $('.current').on('click', function() {
-        $container.css('display','');
-        
+        if ($container.css('display')=== 'none') {
+            $container.css('display','');
+            $('.current').text('Select a value');
+        } else {
+            $container.css('display','none');
+        }   
     });
     $('.dropdown-item').on('click', function() {
         $('.current').attr('data-value', $(this).attr('data-value')); 
         $('.current').text($(this).text());            
         $container.css('display','none');
-
-        // this is working by description but not passing the test...
-        var $selected = $selector.children().eq($(this).attr('data-index'));
-        $selected.attr('selected', '');        
+       
         $selector.val($(this).attr('data-value'));
         $selector.attr('value', $selector.val());
-        console.log($selected);
     });
   };
 }
